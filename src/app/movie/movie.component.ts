9e7b4f581f27af6969a986a99b1624c5e90f29a1
zominym <marc.zominy@etu.univ-lyon1.fr>
2017-02-15 18:41:58 +0100

    first test for movies

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,11 +1,30 @@
-import {Component, Input} from '@angular/core';
-import {Movie} from './movie'
+import {Component, OnInit} from '@angular/core';
+import {Movie} from './movie';
+import {MovieService} from './movie.service';
 
 @Component({
-  selector: 'movie',
-  template: require('./movie.html')
+  selector: 'movies',
+  template: require('./movies.html'),
+  providers: [MovieService]
 })
 
-export class MovieComponent {
-  @Input() item: Movie;
+export class MovieComponent implements OnInit {
+  movies: Movie[] = [];
+  errorMessage: string;
+  mode = 'Observable';
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+    console.log(this.movies);
+  }
+
+  getMovies() {
+    this.movieService.getMovies()
+      .subscribe(
+      movies => this.movies = movies,
+      error => this.errorMessage = <any>error);
+  }
+
 }
